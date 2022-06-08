16511858ea5b3c8a1da63ebfd8a161c9dd30554f
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-08 19:19:49 +0600

    Add different types of routing for single application

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <h1>App-1</h1>
       <App />
     </BrowserRouter>
+    <MemoryRouter>
+      <h1>App-2</h1>
+      <App />
+    </MemoryRouter>
+    <HashRouter>
+      <h1>App-3</h1>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
