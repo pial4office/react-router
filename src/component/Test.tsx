2e27c29aadd95c38d457e486d565481cd3baaa35
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-09 15:13:54 +0600

    Add link component

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -6,6 +6,11 @@ export default function Test() {
       <Link to="hello">hello without / before path</Link>
       <br />
       <Link to="/hello">hello with /</Link>
+      <Link to="/hello" reloadDocument>
+        hello with reload
+      </Link>
+      <br />
+      <Link to="..">.. in link</Link>
     </div>
   );
 }
